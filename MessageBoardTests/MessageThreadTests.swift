131307a92d4228b034faa3d4062944cbecf9fcd5
Craig Swanson <cswanson02@me.com>
2020-02-10 11:34:59 -0600

    jotted down list of testing assertions in pseudo code.

--- MessageBoardTests/MessageThreadTests.swift
+++ MessageBoardTests/MessageThreadTests.swift
@@ -11,5 +11,14 @@ import XCTest
 
 class MessageThreadTests: XCTestCase {
     
-    
+    // # cells equals message threads count
+    // typing in text box and pressing enter adds a cell with that text
+    // tapping on a cell brings the detail VC for that message thread
+    // messages, if any, are cells in the detail VC
+    // tapping back pops VC off stack
+    // tapping plus adds new message view to stack
+    // tapping send with empty fields should prevent creating a new message (it currently does not)
+    // tapping send with populated fields creates a new message and pops the view off stack
+    // the detail VC now has an additional cell with the new message
+    //
 }
